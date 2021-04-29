933021dc7d5ddb5564500a18c5a22328b1e853b8
IanAragonS <a01177009@itesm.mx>
2021-04-29 15:05:33 -0500

    lab05

--- Lab03/app.js
+++ Lab03/app.js
@@ -4,10 +4,34 @@ var cors = require('cors');
 const port = 3000;
 const axios = require('axios').default;
 app.use(cors());
+var MongoClient = require('mongodb').MongoClient
+var collection;
+
+const uri = "mongodb+srv://wanderer:holajaja9889@cluster0.6qswt.mongodb.net/pokemonDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true}, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1});
+client.connect(err => {
+	collection = client.db("pokemonDB").collection("pokemons"); 
+});
 
 app.get('/queryForm', (req, res) => {
     res.sendFile("./page.html", {root: __dirname});
 });
+
+app.route('/pokemons').get(async function(req, res){  
+    let pokemons = await collection.find().toArray();
+    res.send(pokemons);
+});
+
+app.post('/queryForm/pokemon', function (req, res) {
+
+    let nameParam = req.query.name;
+	collection.deleteOne({
+		name: nameParam
+	}, function(err, obj) {
+		if (err) throw err;
+		console.log("1 document deleted");
+	});
+})
 
 app.get('/queryForm/pokemon', function (req, res) {
 
@@ -18,7 +42,6 @@ app.get('/queryForm/pokemon', function (req, res) {
     axios.get(URL+nameParam)
     .then(pokemon_response => {
 		var pokemonData = pokemon_response.data;
-		//pokemonData = JSON.parse(pokemonData);
 
 		var type = pokemonData.types["0"].type.name;
 		var weigth =  pokemonData.weight;
@@ -26,6 +49,15 @@ app.get('/queryForm/pokemon', function (req, res) {
 		var name = pokemonData.name;
 		
 		var str = JSON.stringify({name: name, img: img, weigth: weigth, type: type});
+		collection.insertOne({
+			name: name,
+			img: img,
+			weigth: weigth,
+			type: type
+		}, function(err, res) {
+			if (err) throw err;
+			console.log("1 document inserted");
+		});
 		
         console.log("I am making a request to " + URL);
         res.send(str);
